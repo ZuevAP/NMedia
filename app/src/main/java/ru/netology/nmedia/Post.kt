09e7386d6f38e6da54e391e69e1a52fe3354f38f
Zuev Artem <zuevap@gmail.com>
2022-05-10 11:13:55 +0300

    Task4

---
+++ app/src/main/java/ru/netology/nmedia/Post.kt
@@ -0,0 +1,26 @@
+package ru.netology.nmedia
+
+import java.text.DecimalFormat
+
+data class Post (
+    val id: Long,
+    val author: String,
+    val content: String,
+    val published: String,
+    val likes: Int = 0,
+    var likedByMe: Boolean = false
+) {
+
+    fun displayNumbers(number: Int): String {
+
+        val decimalFormat = DecimalFormat("#.#")
+        return when (number) {
+            in 0..1099 -> "$number"
+            in 1100..9_999 -> "${decimalFormat.format(number.toFloat() / 1_000)}K"
+            in 10_000..999_999 -> "${number / 1_000}K"
+            else -> "${decimalFormat.format(number.toFloat() / 1_000_000)}M"
+
+        }
+    }
+
+}
